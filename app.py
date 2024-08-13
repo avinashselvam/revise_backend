ba7844d0130fc745733b7b69e7f9462bbc0c83bf
avinash <avinashpanneer@gmail.com>
2024-08-13 23:36:32 +0530

    logging to firebase added

--- app.py
+++ app.py
@@ -1,38 +1,31 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import get_questions_from_open_ai, get_questions_and_evaluations_from_open_ai
+from uuid import uuid4
 
-import time
+from firebase_admin import initialize_app, credentials, firestore
 
 app = Flask(__name__)
 
 CORS(app)
 
+cred = credentials.Certificate("service_account_key.json")
+_ = initialize_app(cred)
+store = firestore.client()
+doc_ref = store.collection(u'user_data')
 
-def get_questions_1(topic):
+def upload_feedback_to_firestore(session_id, feedback):
+    data = { 'feedback': feedback }
+    doc_ref.document(session_id).set(data, merge=True)
 
-    return { 
-        "questions": [{
-            "subtopic": "stupidity",
-            "question": "what the fuck?"
-        }]*10
-    }
+def upload_qa_to_firestore(session_id, difficulty, questions, answers):
+    data = { str(difficulty): { 'questions': questions, 'answers': answers } }
+    doc_ref.document(session_id).set(data, merge=True)
 
-def get_questions_2(topic, submission):
+def upload_topic_to_firestore(session_id, topic):
+    doc_ref.document(session_id).set({'topic': topic})
 
-    return {
-        "questions": [{
-            "subtopic": "stupidity",
-            "question": "what the fuck?"
-        }]*10,
-        "evaluations": [{
-            "status": "correct",
-	        "explanation": "you're stupid",
-	        "reason_if_wrong": "it is not"
-        }]*10
-    }
-
-@app.post("/questions")
+@app.post("/question")
 def get_questions():
 
     payload = request.json
@@ -41,16 +34,34 @@ def get_questions():
     if payload_type == 1:
         topic = payload['topic']
         data = get_questions_from_open_ai(topic)
+        session_id = str(uuid4())
+        response = {**data.model_dump(), 'sessionId': session_id}
+        upload_topic_to_firestore(session_id, topic)
+
     elif payload_type == 2:
         topic = payload['topic']
-        difficulty = 2
+        session_id = payload['sessionId']
+        difficulty = payload['difficulty']
         questions = payload['questions']
         answers = payload['answers']
         submission = "\n".join([f"{question}\n{answer}" for question, answer in zip(questions, answers)])
         data = get_questions_and_evaluations_from_open_ai(topic, difficulty, submission)
-    
-    # return data
-    # print(data.model_dump_json())
+        response = data.model_dump()
+        upload_qa_to_firestore(session_id, difficulty, questions, answers)
 
-    return jsonify(data.model_dump())
+    return jsonify(response)
+
+@app.post("/log")
+def log():
+
+    payload = request.json
+    session_id = payload['sessionId']
+    feedback = payload['feedback']
+
+    upload_feedback_to_firestore(session_id, feedback)
+
+    return "uploaded", 201
+
+
+
     
--- utils.py
+++ utils.py
@@ -28,7 +28,6 @@ class TypeTwoResponse(BaseModel):
 	evaluations: List[Evaluation]
 	questions: List[Question]
 
-
 def get_questions_from_open_ai(topic):
 
     prompt = f"I want to revise {topic} at increasing levels of difficulty from 1 to 4. Ask me 5 varied questions (mention the subtopic) from the topic with difficulty level 1 to get started. response should be in json format."
@@ -48,7 +47,7 @@ def get_questions_from_open_ai(topic):
 
 def get_questions_and_evaluations_from_open_ai(topic, difficulty, submission):
 	
-    prompt = f"You're currently helping me revise {topic} at increasing levels of difficulty from 1 to 4. Below are the previous 5 questions that you asked me and my answers for them {submission}. Evaluate them. If correct, no explanation needed. If wrong, provide reason_if_wrong. If unattempted, provide answer in explanation. And then ask me 5 more questions from the same topic but with increased difficulty level of {difficulty}. response should be in json format."
+    prompt = f"You're currently helping me revise {topic} at increasing levels of difficulty from 1 to 4. Below are the previous 10 questions that you asked me and my answers for them {submission}. Evaluate them. If correct, no explanation needed. If wrong, provide reason_if_wrong. If unattempted, provide answer in explanation. And then ask me 10 more questions from the same topic but with increased difficulty level of {difficulty}. response should be in json format."
 	
     completion = client.beta.chat.completions.parse(
         model=MODEL,
@@ -62,5 +61,30 @@ def get_questions_and_evaluations_from_open_ai(topic, difficulty, submission):
     output = completion.choices[0].message.parsed
 	
     return output
+
+### DUMMY DATA UTILS
+
+def get_questions_1(topic):
+
+    return { 
+        "questions": [{
+            "subtopic": "stupidity",
+            "question": "what the fuck?"
+        }]*10
+    }
+
+def get_questions_2(topic, submission):
+
+    return {
+        "questions": [{
+            "subtopic": "stupidity",
+            "question": "what the fuck?"
+        }]*10,
+        "evaluations": [{
+            "status": "correct",
+	        "explanation": "you're stupid",
+	        "reason_if_wrong": "it is not"
+        }]*10
+    }
 
 
